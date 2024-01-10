8b5e5e1233ddd35d596d1c9162db7a947c823c18
jaerrib <jaerrib@tutanota.com>
2024-01-10 12:46:29 -0500

    Add/update organization views

--- organizations/urls.py
+++ organizations/urls.py
@@ -1,14 +1,36 @@
 from django.urls import path
 
-from .views import HomePageView, OrganizationListView, OrganizationDetailView
+from .views import (
+    HomePageView,
+    OrganizationListView,
+    OrganizationDetailView,
+    OrganizationCreateView,
+    OrganizationUpdateView,
+    OrganizationDeleteView,
+)
 
 urlpatterns = [
-    path("organizations/", OrganizationListView.as_view(),
-         name="organization_list"),
+    path(
+        "organizations/new/",
+        OrganizationCreateView.as_view(),
+        name="organization_new",
+    ),
     path(
         "organizations/<int:pk>/",
         OrganizationDetailView.as_view(),
         name="organization_detail",
     ),
+    path(
+        "organizations/<int:pk>/edit/",
+        OrganizationUpdateView.as_view(),
+        name="organization_edit",
+    ),
+    path(
+        "organizations/<int:pk>/delete/",
+        OrganizationDeleteView.as_view(),
+        name="organization_delete",
+    ),
+    path("organizations/", OrganizationListView.as_view(),
+         name="organization_list"),
     path("", HomePageView.as_view(), name="home"),
 ]
--- organizations/views.py
+++ organizations/views.py
@@ -1,4 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Organization
 
@@ -15,3 +17,21 @@ class OrganizationListView(ListView):
 class OrganizationDetailView(DetailView):
     model = Organization
     template_name = "organization_detail.html"
+
+
+class OrganizationCreateView(CreateView):
+    model = Organization
+    template_name = "organization_new.html"
+    fields = ["name", "creator", "details"]
+
+
+class OrganizationUpdateView(UpdateView):
+    model = Organization
+    template_name = "organization_edit.html"
+    fields = ["name", "details"]
+
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = "organization_delete.html"
+    success_url = reverse_lazy("organization_list")
